5105acafd3ee63ec1a9d968a7a35d039ed507279
Frankie Vexx <franblog7@gmail.com>
2023-07-06 21:59:21 +0300

    ducktyped notation

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""using sequence
+takes in a safe first element
+    Returns:
+       a list
+"""
+
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
+        typing.Union[typing.Any, None]:
+    """Duck-typed annotation"""
+    if lst:
+        return lst[0]
+    else:
+        return None
+    
