170b5d791619a9924fe9ff44205cc001aee80797
Frankie Vexx <franblog7@gmail.com>
2023-07-06 21:06:24 +0300

    concatenate strings and return the floor of float

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function to concatenate two strings"""
+    return "{}{}".format(str1, str2)
